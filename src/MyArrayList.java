13ad04c26a1445f6c9084d367264d10d7885e31c
Ariston <chenjiechuan521@yeah.net>
2018-06-13 17:21:46 +1000

    finished MyArrayList and MyLinkedList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,26 +1,69 @@
 public class MyArrayList<T> implements List<T> {
+
+    private final int INITIAL_SIZE = 16;
+    private final float GROWTH_FACTOR = 1.5f;
+    private final float THRESHOLD_FACTOR = 0.75f;
+
+    private T[] stock;
+    private int size;
+
+    public MyArrayList(){
+        this.stock = (T[])new Object[INITIAL_SIZE];
+        this.size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        if(size >= stock.length * THRESHOLD_FACTOR){
+            T[] newStock = (T[])new Object[(int)(stock.length * GROWTH_FACTOR)];
+            System.arraycopy(stock, 0, newStock, 0, stock.length);
+            stock = newStock;
+        }
+        stock[size++] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if(index>=size || index<0)
+            throw new ArrayIndexOutOfBoundsException();
+        T output = stock[index];
+        int threshold = (int)(stock.length*THRESHOLD_FACTOR);
+        if(size-1 < threshold){
+            T[] newStock = (T[])new Object[threshold];
+            System.arraycopy(stock, 0, newStock, 0, index);
+            System.arraycopy(stock, index+1, newStock, index, size-index-1);
+            stock = newStock;
+        } else {
+            for (int i = index+1; i < stock.length; i++) {
+                stock[i-1] = stock[i];
+            }
+        }
+        size--;
+        return output;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(index>=size || index<0)
+            throw new ArrayIndexOutOfBoundsException();
+        return stock[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void reverse() {
-
+        int left = 0;
+        int right = size-1;
+        while(left < right){
+            T temp = stock[left];
+            stock[left] = stock[right];
+            stock[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,26 +1,108 @@
 public class MyLinkedList<T> implements List<T>{
+
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public MyLinkedList(){
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    private class Node<T>{
+        T val;
+        Node<T> next;
+        Node<T> last;
+        private Node(T val){
+            this.val = val;
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if(size==0){
+            head = new Node(value);
+            tail = head;
+        } else {
+            tail.next = new Node(value);
+            tail.next.last = tail;
+            tail = tail.next;
+        }
+        size++;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> output = find(index);
+        if(index!=0 && index!=size-1) {
+            output.last.next = output.next;
+            output.next.last = output.last;
+        }
+        if(index==0) {
+            head = output.next;
+            head.last = null;
+        }
+        if(index==size-1){
+            tail = output.last;
+            tail.next = null;
+        }
+        size--;
+        return output.val;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return find(index).val;
+    }
+
+    private Node<T> find(int index){
+        if(index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException();
+        Node<T> output;
+        if(index >= size/2){
+            output = tail;
+            for(int i=0; i<size-index-1; i++)
+                output = output.last;
+        }else{
+            output = head;
+            for(int i=0; i<index; i++)
+                output = output.next;
+        }
+        return output;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void reverse() {
+        if(head==null)
+            return;
+        tail = head;
+        Node<T> prev = null;
+        Node<T> node = head;
+        Node<T> next = head.next;
 
+        node.last = next;
+        node.next = prev;
+
+        prev = node;
+        node = next;
+        while(node.next!=null){
+            next = node.next;
+
+            prev.last = node;
+            node.next = prev;
+
+            prev = node;
+            node = next;
+        }
+        node.next = prev;
+
+        head = node;
+        head.last = null;
     }
 }
